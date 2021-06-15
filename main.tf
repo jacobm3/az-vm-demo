3e5e38129a86335db9619fcc1326de3425522b2f
Jacob <jacobm3@gmail.com>
2021-06-14 22:43:31 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,9 +25,6 @@ resource "azurerm_subnet" "test" {
   resource_group_name  = "${azurerm_resource_group.test.name}"
   virtual_network_name = "${azurerm_virtual_network.test.name}"
   address_prefixes       = ["10.0.2.0/24"]
-  tags = {
-    owner = "jmartinson"
-  }
 }
 
 resource "azurerm_network_interface" "test" {
@@ -42,9 +39,6 @@ resource "azurerm_network_interface" "test" {
     subnet_id                     = "${azurerm_subnet.test.id}"
     private_ip_address_allocation = "Static"
     private_ip_address            = "${element(var.ip_addresses, count.index)}"
-  }
-  tags = {
-    owner = "jmartinson"
   }
 }
 
